7134dddb686ee2828896d177442cbb7063587d41
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-06-23 20:59:36 -0500

    Split project

--- evaluate_cifar10_bias.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./train.py --model ai84net5 --dataset CIFAR10 --confusion --evaluate --ai84 --exp-load-weights-from trained/ai84-cifar10-bias.pth.tar -8 --use-bias
--- evaluate_fashionmnist.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./train.py --model ai84net5 --dataset FashionMNIST --confusion --evaluate --ai84 --exp-load-weights-from trained/ai84-fashionmnist.pth.tar -8
