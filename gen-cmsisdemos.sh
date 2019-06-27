f8fff63833fdce16a2aced71c3bb0b4c6aa4e8b4
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-06-27 13:42:33 -0500

    Move network description files to separate directories

--- gen-cmsisdemos.sh
+++ gen-cmsisdemos.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./cnn-gen.py --top-level cnn --test-dir demos --prefix CIFAR-10-Arm --checkpoint-file trained/ai84-cifar10.pth.tar --config-file cifar10-hwc.yaml --fc-layer --embedded-code --cmsis-software-nn
-./cnn-gen.py --top-level cnn --test-dir demos --prefix MNIST-ExtraSmall-Arm --checkpoint-file trained/ai84-mnist-extrasmallnet.pth.tar --config-file mnist-chw-extrasmallnet.yaml --fc-layer --embedded-code --cmsis-software-nn
-./cnn-gen.py --top-level cnn --test-dir demos --prefix MNIST-Small-Arm --checkpoint-file trained/ai84-mnist-smallnet.pth.tar --config-file mnist-chw-smallnet.yaml --fc-layer --embedded-code --cmsis-software-nn
+./cnn-gen.py --top-level cnn --test-dir demos --prefix CMSIS-CIFAR-10 --checkpoint-file trained/ai84-cifar10.pth.tar --config-file networks/cifar10-hwc.yaml --fc-layer --embedded-code --cmsis-software-nn
+./cnn-gen.py --top-level cnn --test-dir demos --prefix CMSIS-MNIST-ExtraSmall --checkpoint-file trained/ai84-mnist-extrasmallnet.pth.tar --config-file networks/mnist-chw-extrasmallnet.yaml --fc-layer --embedded-code --cmsis-software-nn
+./cnn-gen.py --top-level cnn --test-dir demos --prefix CMSIS-MNIST-Small --checkpoint-file trained/ai84-mnist-smallnet.pth.tar --config-file networks/mnist-chw-smallnet.yaml --fc-layer --embedded-code --cmsis-software-nn
--- gen-rtlsims-ai85.sh
+++ gen-rtlsims-ai85.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-./cnn-gen.py --verbose --autogen tests --top-level cnn -L --test-dir tests --prefix ai85-cifar-bias --checkpoint-file trained/ai85-cifar10-bias.pth.tar --config-file cifar10-hwc.yaml --stop-after 0 --ai85
-./cnn-gen.py --verbose --autogen tests --top-level cnn -L --test-dir tests --prefix ai85-q4-cifar-bias --checkpoint-file trained/ai85-cifar10-bias-quant4.pth.tar --config-file test-ai85-cifar10-hwc-quant4.yaml --stop-after 0 --ai85
-./cnn-gen.py --verbose --autogen tests --top-level cnn -L --test-dir tests --prefix ai85-q4-cifar-bias --checkpoint-file trained/ai85-cifar10-bias-quant4.pth.tar --config-file test-ai85-cifar10-hwc-quant4.yaml --ai85
+./cnn-gen.py --verbose --autogen rtlsim --top-level cnn -L --test-dir rtlsim --prefix ai85-cifar-bias --checkpoint-file trained/ai85-cifar10-bias.pth.tar --config-file networks/cifar10-hwc.yaml --stop-after 0 --ai85
+./cnn-gen.py --verbose --autogen rtlsim --top-level cnn -L --test-dir rtlsim --prefix ai85-q4-cifar-bias --checkpoint-file trained/ai85-cifar10-bias-quant4.pth.tar --config-file tests/test-ai85-cifar10-hwc-quant4.yaml --stop-after 0 --ai85
+./cnn-gen.py --verbose --autogen rtlsim --top-level cnn -L --test-dir rtlsim --prefix ai85-q4-cifar-bias --checkpoint-file trained/ai85-cifar10-bias-quant4.pth.tar --config-file tests/test-ai85-cifar10-hwc-quant4.yaml --ai85
 
-./cnn-gen.py --verbose --autogen tests --top-level cnn -L --test-dir tests --prefix ai85-mnist-extrasmall --checkpoint-file trained/ai84-mnist-extrasmallnet.pth.tar --config-file mnist-chw-extrasmallnet.yaml --stop-after 2 --ai85
-./cnn-gen.py --verbose --autogen tests --top-level cnn -L --test-dir tests --prefix ai85-q4-16x16avgpool --checkpoint-file trained/ai85-cifar10-bias-quant4.pth.tar --config-file test-ai85-cifar10-hwc-16x16avgpool.yaml --stop-after 0 --ai85
-./cnn-gen.py --verbose --autogen tests --top-level cnn -L --test-dir tests --prefix ai85-3x3s2p2avgpool --checkpoint-file trained/ai85-cifar10-bias.pth.tar --config-file test-ai85-cifar10-hwc-3x3s2p2avgpool.yaml --stop-after 0 --ai85
+./cnn-gen.py --verbose --autogen rtlsim --top-level cnn -L --test-dir rtlsim --prefix ai85-mnist-extrasmall --checkpoint-file trained/ai84-mnist-extrasmallnet.pth.tar --config-file networks/mnist-chw-extrasmallnet.yaml --stop-after 2 --ai85
+./cnn-gen.py --verbose --autogen rtlsim --top-level cnn -L --test-dir rtlsim --prefix ai85-q4-16x16avgpool --checkpoint-file trained/ai85-cifar10-bias-quant4.pth.tar --config-file tests/test-ai85-cifar10-hwc-16x16avgpool.yaml --stop-after 0 --ai85
+./cnn-gen.py --verbose --autogen rtlsim --top-level cnn -L --test-dir rtlsim --prefix ai85-3x3s2p2avgpool --checkpoint-file trained/ai85-cifar10-bias.pth.tar --config-file tests/test-ai85-cifar10-hwc-3x3s2p2avgpool.yaml --stop-after 0 --ai85
 
-./cnn-gen.py --verbose --autogen tests --top-level cnn -L --test-dir tests --prefix ai85-3x3s1avgpool --checkpoint-file trained/ai84-mnist-extrasmallnet.pth.tar --config-file test-pooling3x3s1.yaml --stop-after 0 --ai85
-./cnn-gen.py --verbose --autogen tests --top-level cnn -L --test-dir tests --prefix ai85-4x4s2avgpool --checkpoint-file trained/ai84-mnist-extrasmallnet.pth.tar --config-file test-pooling4x4s2.yaml --stop-after 0 --ai85
+./cnn-gen.py --verbose --autogen rtlsim --top-level cnn -L --test-dir rtlsim --prefix ai85-3x3s1avgpool --checkpoint-file trained/ai84-mnist-extrasmallnet.pth.tar --config-file tests/test-pooling3x3s1.yaml --stop-after 0 --ai85
+./cnn-gen.py --verbose --autogen rtlsim --top-level cnn -L --test-dir rtlsim --prefix ai85-4x4s2avgpool --checkpoint-file trained/ai84-mnist-extrasmallnet.pth.tar --config-file tests/test-pooling4x4s2.yaml --stop-after 0 --ai85
 
-./cnn-gen.py --verbose --autogen tests --top-level cnn -L --test-dir tests --prefix ai85-wideout --checkpoint-file trained/ai85-mnist-wide.pth.tar --config-file test-ai85-mnistwide.yaml --stop-after 0 --ai85
+./cnn-gen.py --verbose --autogen rtlsim --top-level cnn -L --test-dir rtlsim --prefix ai85-wideout --checkpoint-file trained/ai85-mnist-wide.pth.tar --config-file tests/test-ai85-mnistwide.yaml --stop-after 0 --ai85
 
