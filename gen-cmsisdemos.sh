fbea5987a606d84d716401063656e2518629c952
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-07-12 10:55:44 -0500

    Add CMSIS SpeechCom demo

--- gen-cmsisdemos.sh
+++ gen-cmsisdemos.sh
@@ -3,3 +3,4 @@
 ./cnn-gen.py --top-level cnn --test-dir demos --prefix CMSIS-MNIST-ExtraSmall --checkpoint-file trained/ai84-mnist-extrasmallnet.pth.tar --config-file networks/mnist-chw-extrasmallnet.yaml --fc-layer --embedded-code --cmsis-software-nn
 ./cnn-gen.py --top-level cnn --test-dir demos --prefix CMSIS-MNIST-Small --checkpoint-file trained/ai84-mnist-smallnet.pth.tar --config-file networks/mnist-chw-smallnet.yaml --fc-layer --embedded-code --cmsis-software-nn
 ./cnn-gen.py --top-level cnn --test-dir demos --prefix CMSIS-MNIST --checkpoint-file trained/ai84-mnist.pth.tar --config-file networks/mnist-chw.yaml --fc-layer --embedded-code --cmsis-software-nn
+./cnn-gen.py --top-level cnn --test-dir demos --prefix CMSIS-SpeechCom --checkpoint-file trained/ai84-speechcom-net7.pth.tar --config-file networks/speechcom-chw.yaml --fc-layer --embedded-code --cmsis-software-nn
