161bd4add3ad75d808ceb7b74b91ac78d4897614
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-07-19 15:24:18 -0500

    Support multi-layer test networks; add energy test network

--- sampleweight.py
+++ sampleweight.py
@@ -27,19 +27,20 @@ def load(dataset, _quantization):
 
     dataset = dataset.lower()
 
-    # Load weights saved using
-    # np.save(f'tests/{dataset}', w.reshape(w.shape[0], w.shape[1], w.shape[-2], w.shape[-1]),
-    #         allow_pickle=False, fix_imports=False)
+    # Load weights saved using:
+    #    w = np.random.randint(-128, 127, (2, 64, 64, 3, 3), dtype=np.int8)
+    #    np.save(f'tests/{dataset}', w, allow_pickle=False, fix_imports=False)
 
-    layers = 1
     w = np.load(os.path.join('tests', f'weights_{dataset}.npy'),
                 allow_pickle=False, fix_imports=False)
-    output_channels.append(w.shape[0])  # Output channels
-    input_channels.append(w.shape[1])  # Input channels
-    if len(w.shape) == 4:
-        weights.append(w.reshape(-1, w.shape[-2], w.shape[-1]))
-    else:
-        weights.append(w.reshape(-1, w.shape[-1]))
-    bias.append(None)
+    layers = w.shape[0]
+    for ll in range(layers):
+        output_channels.append(w[ll].shape[0])  # Output channels
+        input_channels.append(w[ll].shape[1])  # Input channels
+        if len(w[ll].shape) == 4:
+            weights.append(w[ll].reshape(-1, w[ll].shape[-2], w[ll].shape[-1]))
+        else:
+            weights.append(w[ll].reshape(-1, w[ll].shape[-1]))
+        bias.append(None)
 
     return layers, weights, bias, fc_weights, fc_bias, input_channels, output_channels
