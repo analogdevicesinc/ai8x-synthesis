50ad5db50e5c3f5b8afc962c322924f798f0d491
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-12-18 18:24:53 -0600

    Add test for power sim

--- gen-blocklevel-ai85.sh
+++ gen-blocklevel-ai85.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ./cnn-gen.py --verbose --autogen blocklevel -L --test-dir blocklevel --prefix ai85-cifar --checkpoint-file trained/ai84-cifar10.pth.tar --config-file networks/cifar10-hwc.yaml --ai85
 ./cnn-gen.py --verbose --autogen blocklevel -L --test-dir blocklevel --prefix ai85-singlebyte-hwc --config-file tests/test-singlebyte-hwc.yaml --ai85
+./cnn-gen.py --verbose --autogen blocklevel -L --test-dir blocklevel --prefix ai85-kmax_bmax_dmax --config-file tests/test-max.yaml --ai85
