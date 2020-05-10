bd10d462baa131ab8767a1486aa16c4063c9ada4
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-05-10 15:39:41 -0500

    Add CIFAR-100 demo, sample and trained weights

--- gen-demos-ai85.sh
+++ gen-demos-ai85.sh
@@ -9,6 +9,6 @@ cp device-ai85/*.h $SDK_TARGET/Common/
 
 ./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix mnist --checkpoint-file trained/ai85-mnist.pth.tar --config-file networks/mnist-chw-ai85.yaml --device 85 --compact-data --mexpress --softmax --display-checkpoint $CLOCK_TRIM
 ./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix mnist-riscv --checkpoint-file trained/ai85-mnist.pth.tar --config-file networks/mnist-chw-ai85.yaml --device 85 --compact-data --mexpress --softmax --display-checkpoint $CLOCK_TRIM --riscv --riscv-flash --riscv-cache --riscv-debug
-./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix cifar-10 --checkpoint-file trained/ai85-cifar10-bias.pth.tar --config-file networks/cifar10-hwc.yaml --device 85 --verify-writes --compact-data --mexpress $CLOCK_TRIM
+./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix cifar-10 --checkpoint-file trained/ai85-cifar10-bias.pth.tar --config-file networks/cifar10-hwc.yaml --device 85 --compact-data --mexpress --display-checkpoint $CLOCK_TRIM
+./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix cifar-100 --checkpoint-file trained/ai85-cifar100.pth.tar --config-file networks/cifar100-simple.yaml --device 85 --compact-data --mexpress --softmax --display-checkpoint $CLOCK_TRIM
 
-# ./ai8xize.py -e --verbose --top-level cnn -L --test-dir demos --prefix ai85-fifostream-cifar2-hwc --checkpoint-file trained/ai84-cifar10.pth.tar --config-file tests/test-fifostream-cifar-hwc.yaml --device 85 --fifo $CLOCK_TRIM
