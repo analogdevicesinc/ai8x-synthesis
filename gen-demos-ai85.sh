6c648008630c6a400486a82b0ff572f7e2251e6d
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-05-19 14:55:19 -0500

    KWS20: quantization script

--- gen-demos-ai85.sh
+++ gen-demos-ai85.sh
@@ -11,4 +11,4 @@ cp device-ai85/*.h $SDK_TARGET/Common/
 ./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix mnist-riscv --checkpoint-file trained/ai85-mnist.pth.tar --config-file networks/mnist-chw-ai85.yaml --device 85 --compact-data --mexpress --softmax --display-checkpoint $CLOCK_TRIM --riscv --riscv-flash --riscv-cache --riscv-debug
 ./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix cifar-10 --checkpoint-file trained/ai85-cifar10-bias.pth.tar --config-file networks/cifar10-hwc.yaml --device 85 --compact-data --mexpress --display-checkpoint $CLOCK_TRIM
 ./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix cifar-100 --checkpoint-file trained/ai85-cifar100.pth.tar --config-file networks/cifar100-simple.yaml --device 85 --compact-data --mexpress --softmax --display-checkpoint $CLOCK_TRIM --boost 2.5
-
+./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix kws20 --checkpoint-file trained/ai85-kws20.pth.tar --config-file networks/kws20-hwc.yaml --device 85 --compact-data --mexpress --softmax --embedded-code --display-checkpoint $CLOCK_TRIM
