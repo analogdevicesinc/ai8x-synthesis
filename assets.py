77a9522c834a344b9b4c68f75830153538d9fbbb
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-06-05 13:36:13 -0500

    Improved support for Eclipse

--- assets.py
+++ assets.py
@@ -26,3 +26,30 @@ def copy(
     for _, _, files in sorted(os.walk(os.path.join(base, source))):
         for name in sorted(files):
             shutil.copy(os.path.join(base, source, name), dst)
+
+
+def eclipse_template(
+        base,
+        source,
+        target,
+        test_name,
+):
+    """
+    Copy all files `base`/`source` to `target`/`test_name`, with file name and
+    content substitution.
+    """
+    template = 'template'
+
+    for _, _, files in sorted(os.walk(os.path.join(base, source))):
+        for name in sorted(files):
+            if name.startswith(template):
+                dst = os.path.join(
+                    target,
+                    test_name,
+                    name[len(template):].replace('##__PROJ_NAME__##', test_name),
+                )
+                with open(os.path.join(base, source, name)) as infile, open(dst, 'w+') as outfile:
+                    for line in infile:
+                        outfile.write(line.replace('##__PROJ_NAME__##', test_name))
+            else:
+                shutil.copy(os.path.join(base, source, name), os.path.join(target, test_name))
