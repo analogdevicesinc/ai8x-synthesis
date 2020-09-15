817660c420ef6c4eda69fb6d4fa45ee096ac0451
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-09-15 16:55:00 -0500

    Merge conflicts

--- gen-demos-max78000.sh
+++ gen-demos-max78000.sh
@@ -2,10 +2,12 @@
 CLOCK_TRIM=""
 # Example: CLOCK_TRIM="--clock-trim 0x173,0x5e,0x14"
 SDK_TARGET="sdk/Examples/MAX78000/CNN"
+
 # Update common code and header files
 mkdir -p $SDK_TARGET/Common/
 cp device-ai85/softmax.c $SDK_TARGET/Common/
 cp device-ai85/*.h $SDK_TARGET/Common/
+
 ./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix mnist --checkpoint-file trained/ai85-mnist.pth.tar --config-file networks/mnist-chw-ai85.yaml --device MAX78000 --compact-data --mexpress --softmax --display-checkpoint $CLOCK_TRIM $@
 ./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix mnist-riscv --checkpoint-file trained/ai85-mnist.pth.tar --config-file networks/mnist-chw-ai85.yaml --device MAX78000 --compact-data --mexpress --softmax --display-checkpoint $CLOCK_TRIM --riscv --riscv-flash --riscv-cache --riscv-debug $@
 ./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix cifar-10 --checkpoint-file trained/ai85-cifar10.pth.tar --config-file networks/cifar10-hwc-ai85.yaml --device MAX78000 --compact-data --mexpress --display-checkpoint $CLOCK_TRIM $@
