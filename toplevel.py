f0cabb0a05a2f3f943b6a58e05b321fe1e271b43
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-09-25 16:38:41 -0500

    Fix clock selection comment

--- toplevel.py
+++ toplevel.py
@@ -288,14 +288,14 @@ def main(
                     memfile.write('  MXC_GCR->pclkdiv &= ~(MXC_F_GCR_PCLKDIV_CNNCLKDIV | '
                                   'MXC_F_GCR_PCLKDIV_CNNCLKSEL);\n'
                                   '  MXC_GCR->pclkdiv |= MXC_S_GCR_PCLKDIV_CNNCLKDIV_DIV1; '
-                                  ' // CNN clock: 100 MHz div 2\n'
+                                  ' // CNN clock: 0.5*100 MHz div 1\n'
                                   '  MXC_SYS_ClockEnable(MXC_SYS_PERIPH_CLOCK_CNN); '
                                   '// Enable CNN clock\n')
                 else:
                     memfile.write('  MXC_GCR->pclkdiv &= ~(MXC_F_GCR_PCLKDIV_CNNCLKDIV | '
                                   'MXC_F_GCR_PCLKDIV_CNNCLKSEL);\n'
                                   '  MXC_GCR->pclkdiv |= MXC_S_GCR_PCLKDIV_CNNCLKDIV_DIV8; '
-                                  '// CNN clock: 7.37 MHz div 8\n'
+                                  '// CNN clock: 0.5*7.37 MHz div 8\n'
                                   '  MXC_SYS_ClockEnable(MXC_SYS_PERIPH_CLOCK_CNN); '
                                   '// Enable CNN clock\n')
 
