c6cb89b98389e0a052c8381d7839efb43cc30992
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-10-10 11:13:52 -0500

    Modify scripts to pass shellcheck

--- rtldev/gen-cifar100-ressimplenet-ai85.sh
+++ rtldev/gen-cifar100-ressimplenet-ai85.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 CLOCK_TRIM=""
 # Example: CLOCK_TRIM="--clock-trim 0x173,0x5e,0x14"
+DEVICE="--device MAX78000"
 SDK_TARGET="sdk/Examples/MAX78000/CNN"
 
 # Update common code and header files
@@ -8,4 +9,4 @@ mkdir -p $SDK_TARGET/Common/
 cp assets/device-ai85/softmax.c $SDK_TARGET/Common/
 cp assets/device-ai85/*.h $SDK_TARGET/Common/
 
-./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix cifar-100-ressimplenet --checkpoint-file trained/ai85-cifar100-ressimplenet.pth.tar --config-file networks/cifar100-ressimplenet.yaml --device 85 --compact-data --mexpress --display-checkpoint $CLOCK_TRIM $@
+./ai8xize.py -e --verbose --top-level cnn -L --test-dir $SDK_TARGET --prefix cifar-100-ressimplenet --checkpoint-file trained/ai85-cifar100-ressimplenet.pth.tar --config-file networks/cifar100-ressimplenet.yaml "$DEVICE" MAX78000 --compact-data --mexpress --display-checkpoint "$CLOCK_TRIM" "$@"
