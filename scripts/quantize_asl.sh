c5deb4389d52745fb81c40e724fa3cd3735720e7
Jack Warren <Jack.Warren@maximintegrated.com>
2021-09-02 15:29:08 -0700

    added tars

--- scripts/quantize_asl.sh
+++ scripts/quantize_asl.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#./quantize.py ../ai8x-training/logs/2021.07.09-172633/checkpoint.pth.tar trained/ai85-asl01-chw.pth.tar --device MAX78000 -v -c networks/asl-chw.yaml "$@"
-
-./quantize.py ../ai8x-training/logs/2021.08.06-164826/qat_best.pth.tar trained/ai85-asl01-chw.pth.tar --device MAX78000 -v -c networks/asl-chw.yaml "$@"
+./quantize.py ../ai8x-training/logs/2021.07.23-131712/ai85-asl-qat8.pth.tar trained/ai85-asl-qat8-q.pth.tar --device MAX78000 -v -c networks/asl-chw.yaml "$@"
 
 
