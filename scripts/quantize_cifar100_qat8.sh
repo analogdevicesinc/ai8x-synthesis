42a096007c89559f247cf8a828cf96876d625e23
Robert Muchsel <robert.muchsel@maximintegrated.com>
2021-10-19 10:30:15 -0500

    Improve Windows support
    Minor fix for demo script
    
    Windows

--- scripts/quantize_cifar100_qat8.sh
+++ scripts/quantize_cifar100_qat8.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 quantize.py trained/ai85-cifar100-qat8.pth.tar trained/ai85-cifar100-qat8-q.pth.tar --device MAX78000 -v -c networks/cifar100-simple.yaml "$@"
+python quantize.py trained/ai85-cifar100-qat8.pth.tar trained/ai85-cifar100-qat8-q.pth.tar --device MAX78000 -v -c networks/cifar100-simple.yaml "$@"
