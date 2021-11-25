7e5531035315ce4bd111e4f54d22ac35505e0b43
Victor Loginov <victor.loginov@maximintegrated.com>
2021-11-24 18:13:59 -0600

    Cifar100 efficient net v.2 example

---
+++ scripts/quantize_cifar100_effnet2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python quantize.py trained/ai87-cifar100-effnet2-qat8.pth.tar trained/ai87-cifar100-effnet2-qat8-q.pth.tar --device MAX78000 -v -c networks/ai87-cifar100-effnet2.yaml "$@"
