af6156dc3ab837b1c90a06dc45422fed314ab9a0
Afshin Niktash <48036488+aniktash@users.noreply.github.com>
2022-05-10 07:43:13 -0700

    Removed unnecessary FIFO in cats-vs-dogs for MAX78002 (#235)

--- scripts/gen_catsdogs_max78002.sh
+++ scripts/gen_catsdogs_max78002.sh
@@ -3,4 +3,4 @@ DEVICE="MAX78002"
 TARGET="sdk/Examples/$DEVICE/CNN"
 COMMON_ARGS="--device $DEVICE --compact-data --mexpress --timer 0 --display-checkpoint --verbose"
 
-python ai8xize.py --test-dir $TARGET --prefix cats-dogs --checkpoint-file trained/ai85-catsdogs-qat8-q.pth.tar --config-file networks/cats-dogs-hwc.yaml --fifo --softmax $COMMON_ARGS "$@"
+python ai8xize.py --test-dir $TARGET --prefix cats-dogs --checkpoint-file trained/ai85-catsdogs-qat8-q.pth.tar --config-file networks/cats-dogs-hwc-no-fifo.yaml --softmax $COMMON_ARGS "$@"
