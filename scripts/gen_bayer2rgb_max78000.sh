a49af7a92546d150d53e7740e328e6014cb7dbfa
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-09-11 15:47:39 +0300

    Update gen_bayer2rgb_max78000.sh

--- scripts/gen_bayer2rgb_max78000.sh
+++ scripts/gen_bayer2rgb_max78000.sh
@@ -3,4 +3,4 @@ DEVICE="MAX78000"
 TARGET="sdk/Examples/$DEVICE/CNN"
 COMMON_ARGS="--device $DEVICE --timer 0 --display-checkpoint --verbose"
 
-python ai8xize.py --test-dir sdk/Examples/$DEVICE/CNN --prefix bayer2rgb --checkpoint-file /trained/ai85-b2rgb-qat8-q.pth.tar --config-file networks/ai85_bayer2rgb.yaml --fifo --sample-input tests/sample_imagenet_bayer.npy $COMMON_ARGS "$@"
+python ai8xize.py --test-dir $TARGET --prefix bayer2rgb --checkpoint-file /trained/ai85-b2rgb-qat8-q.pth.tar --config-file networks/ai85_bayer2rgb.yaml --fifo --sample-input tests/sample_imagenet_bayer.npy $COMMON_ARGS "$@"
