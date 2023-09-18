10a366d402a41b5215122d849d1c5ac31d59f0d4
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-09-18 16:12:28 +0300

    Add Bayer2RGB model (#304)

--- scripts/quantize_kws20_v2_ai87.sh
+++ scripts/quantize_kws20_v2_ai87.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 quantize.py trained/ai87-kws20_v2-qat8.pth.tar trained/ai87-kws20_v2-qat8-q.pth.tar --device MAX78000 -v "$@"
+python quantize.py trained/ai87-kws20_v2-qat8.pth.tar trained/ai87-kws20_v2-qat8-q.pth.tar --device MAX78000 -v "$@"
