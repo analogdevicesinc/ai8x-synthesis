7f77353e694ea1b7b420ccc0d722f696ead7f7dd
Oğuzhan Büyüksolak <oguzhan.buyuksolak@analog.com>
2024-10-16 16:00:19 +0300

    Key fix for effnetv2

--- scripts/gen_effnet2_max78002.sh
+++ scripts/gen_effnet2_max78002.sh
@@ -3,4 +3,4 @@ DEVICE="MAX78002"
 TARGET="sdk/Examples/$DEVICE/CNN"
 COMMON_ARGS="--device $DEVICE --timer 0 --display-checkpoint --verbose"
 
-python ai8xize.py --test-dir $TARGET --prefix effnetv2_imagenet --scale-output --softmax --checkpoint-file trained/ai87-imagenet-effnet2-q.pth.tar --config-file networks/ai87-imagenet-effnet2.yaml $COMMON_ARGS "$@"
+python ai8xize.py --test-dir $TARGET --prefix effnetv2_imagenet --softmax --checkpoint-file trained/ai87-imagenet-effnet2-q.pth.tar --config-file networks/ai87-imagenet-effnet2.yaml $COMMON_ARGS "$@"
